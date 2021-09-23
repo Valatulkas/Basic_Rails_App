067507e2cbf5ed283544ff67db1f0c7f752903a7
jmferg11 <jfergie93@gmail.com>
2021-09-23 15:12:23 -0400

    add post model and controller, add initial rspec files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/posts', to: 'posts#index'
   post '/users', to: 'users#create'
+  post '/posts', to: 'posts#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_004206) do
+ActiveRecord::Schema.define(version: 2021_09_23_190340) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.text "content"
   end
 
   create_table "users", force: :cascade do |t|
